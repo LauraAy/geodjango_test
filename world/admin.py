ae8ccc448d1429aca7991af746f6a5c4b8985916
laura_ay <laura.ay1564@gmail.com>
2025-07-09 17:45:17 -0700

    correctin

--- world/admin.py
+++ world/admin.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 from django.contrib.gis import admin
-from ..geodjango.models import WorldBorder
+from .models import WorldBorder
 
 admin.site.register(WorldBorder, admin.ModelAdmin)
-=======
-from django.contrib import admin
->>>>>>> parent of 52a7e50 (completed tutorial)
-
 # Register your models here.
